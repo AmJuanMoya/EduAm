747f134b1c9b33281d04ea5857be8db8e67c42d4
Am Juan Moya <amjuanmoya@gmail.com>
2025-07-10 22:56:24 -0500

    avances en la navegacion en general, creacion de componentes admin, ajustes y avance en arreglar 'crear matricula'

--- server/routes/r_matricula.js
+++ server/routes/r_matricula.js
@@ -9,8 +9,8 @@ router.get("/datos/matricula/:doc", async (req, res) => {
     try {
         const doc = req.params.doc;
         console.log(`Se recibe informacion de matricula...  ${doc}`);
-        let crud = new matricula
-        const datos = await crud.getMatriculaByDoc(doc);
+        let m = new matricula
+        const datos = await m.getMatriculaByDoc(doc);
         if(datos.length <= 0){
             res.json({error: `No se encontraron matriculas con: ${doc}`})
         }else{
@@ -23,8 +23,28 @@ router.get("/datos/matricula/:doc", async (req, res) => {
     }
 });
 
+router.get("/datos/matricula", async(req, res )=>{
+    try{
+        console.log("Se traen matriculas...")
+        let m = new matricula
+        let datos = await m.getall_matriculas()
+        if(datos.length <= 0){ 
+            res.json({error: "No hay matriculas para mostrar"})
+        }else{
+            res.json(datos)
+            console.log("Se envia lista de matriculas")
+        }
+    }catch(err){
+        res.status(500).json({error: "error al traer los datos"})
+        console.log("error al enviar las matriculas, vease...: ", err)
+    }
+})
+
 // --- RUTA POST PARA CREAR MATRÍCULAS ---
-router.post("/crear", async (req, res) => {
+router.post("/datos/matricula", async (req, res) => {
+    if(!req.body.id_matricula){
+        req.body.id_matricula = null
+    }
     const matriculaData = req.body;
     if (!matriculaData.primer_nombre_estudiante || !matriculaData.documento_estudiante || !matriculaData.primer_apellido_estudiante) {
         return res.status(400).json({ message: 'Error: Faltan campos requeridos del estudiante.' });
@@ -32,7 +52,9 @@ router.post("/crear", async (req, res) => {
     if (!matriculaData.nombre_acudiente1 || !matriculaData.numero_documento_acudiente1 || !matriculaData.correo_acudiente1) {
         return res.status(400).json({ message: 'Error: Faltan campos requeridos del acudiente.' });
     }
+
     try {
+        console.log(req.body)
         const newMatriculaId = await new matricula().createMatricula(matriculaData);
         res.status(201).json({
             message: ' 😀​ Matrícula registrada exitosamente ✅​.',
