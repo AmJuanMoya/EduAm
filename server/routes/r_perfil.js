964b1593a170093dd1d6b49f310ecb410f671c76
Cristian Quiroz <camilomonroy400@gmail.com>
2025-08-29 08:18:45 -0500

    Realizacion de login

--- server/routes/r_perfil.js
+++ server/routes/r_perfil.js
@@ -1,43 +1,46 @@
-
 import express from "express";
 import Database from "../../model/database/conectionDB.js"; 
 
 const router = express.Router();
-const db = new Database
+const db = new Database;
+
 router.get("/:id/:tipo", async (req, res) => {
   const { id, tipo } = req.params;
 
   try {
     let query = "";
     let values = [id];
-    let user = null;
 
-    // if (tipo === "estudiante") {
-    //   query = "SELECT id_docuestudiante AS id, estu_nombre AS nombre, estu_apellido AS apellido, est_correo AS correo, estu_rol AS rol FROM t_estudiantes WHERE id_docuestudiante = ?";
-    // } else if (tipo === "empleado") {
-    //   query = "SELECT id_documento_empleado AS id, empl_nombre AS nombre, empl_apellido AS apellido, empl_correo AS correo, empl_rol AS rol FROM t_empleados WHERE id_documento_empleado = ?";
-    // } else {
-    //   return res.status(400).json({ error: "Tipo no válido" });
-    // }
+    if (tipo === "estudiante") {
+      query = `
+        SELECT e.id_docuestudiante AS id, e.estu_nombre AS nombre, e.estu_apellido AS apellido, e.est_correo AS correo, r.rol_descripcion AS rol
+        FROM t_estudiantes e
+        JOIN t_rol r ON e.estu_rol = r.id_rol
+        WHERE e.id_docuestudiante = ?
+      `;
+    } else if (tipo === "empleado") {
+      query = `
+        SELECT emp.id_documento_empleado AS id, emp.empl_nombre AS nombre, emp.empl_apellido AS apellido, emp.empl_correo AS correo, r.rol_descripcion AS rol
+        FROM t_empleados emp
+        JOIN t_rol r ON emp.empl_rol = r.id_rol
+        WHERE emp.id_documento_empleado = ?
+      `;
+    } else {
+      return res.status(400).json({ error: "Tipo no válido" });
+    }
 
-    query = "SELECT * FROM t_estudiantes"
+    const rows = await db.consultar(query, values);
 
-    const rows = await db.consultar(query)
-    
+    if (!rows || rows.length === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
 
-    // if (rows.length === 0) {
-    //   return res.status(404).json({ error: "Usuario no encontrado" });
-    // }
-
-    // user = rows[0];
-    // res.json(user);
-
-    res.json(rows)
-
+    res.json(rows[0]);
   } catch (error) {
     console.error("Error en /api/perfil:", error);
-    res.status(500).json({ error: "Error interno del servidor de cristian" });
+    res.status(500).json({ error: "Error interno del servidor" });
   }
 });
 
 export default router;
+
