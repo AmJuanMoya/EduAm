f9f2985c86d15bcd188952e6e6c2bc6e124a29ca
Am Juan Moya <amjuanmoya@gmail.com>
2025-09-02 11:33:10 -0500

    se integran modulos y se prueban datos en sesion

--- server/app.js
+++ server/app.js
@@ -1,25 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 // import dotenv from 'dotenv';
-
+// import TablasRouter from './routes/r_tablas.js';
 import actividadRouter from './routes/r_actividad.js';
 import informesRouter from './routes/r_informes.js';
 import crearActividadRouter from './routes/r_creacion_actividad.js';
 import exampleRouter from './routes/r_example.js';
-// import dotenv from 'dotenv';
-
-// import TablasRouter from './routes/r_tablas.js';
-import exampleRouter from './routes/r_example.js';
 import matriculaRouter from './routes/r_matricula.js';
 import perfilRouter from "./routes/r_perfil.js"; 
-import TablasRouter from './routes/r_tablas.js';
-import matriculasRouter from "./routes/r_matricula.js";
-import TipoIdentificacionRouter from './routes/r_tipo_identificacion.js';
-
-import actividadesRouter from "./routes/r_actividades.js";
-import actividadDetalleRouter from "./routes/r_actividad_detalle.js";
-import crearActividadRouter from './routes/r_creacion_actividad.js';
-import categoriasRouter from './routes/r_categorias.js';
 import rolesRouter from './routes/r_roles.js';
 import especialidadRouter from './routes/r_especialidad.js';
 import loginRouter from './routes/r_login.js';
@@ -29,16 +17,16 @@ import acudienteRouter from './routes/r_acudientes.js';
 const app = express()
 app.use(cors());
 app.use("/api",express.json());
-app.use("/api/actividad", actividadRouter);
-app.use("/api/informes", informesRouter);
+
 
 
 // rutas
 // app.use('/api', TablasRouter)
-app.use("/api", loginRouter);
+app.use("/api", loginRouter); 
 app.use('/api/example', exampleRouter);
 app.use('/api/actividad', crearActividadRouter);
-
+app.use("/api/actividad", actividadRouter);
+app.use("/api/informes", informesRouter);
 app.use("/api", perfilRouter);
 app.use("/api", rolesRouter);
 app.use("/api", especialidadRouter);
