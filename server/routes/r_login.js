5069624b7af1e8bc4f45844ae0f82a079443c3da
Am Juan Moya <amjuanmoya@gmail.com>
2025-09-22 21:20:17 -0500

    fix validation of session student

--- server/routes/r_login.js
+++ server/routes/r_login.js
@@ -16,6 +16,7 @@ router.post("/login", async (req, res) => {
         }
 
         const result = await Login.validarCredenciales(correo_usuario, contraseña_usuario);
+        console.log('user_id después de la corrección:', result.usuario.id_docuestudiante);
         console.log(result)
 
         if (!result) {
@@ -27,15 +28,17 @@ router.post("/login", async (req, res) => {
             process.env.JWT_SECRET || "supersecretjwtkey", // Deberías usar una variable de entorno para esto
             { expiresIn: '1h' } // El token expira en 1 hora
         );
-
+        // console.log("user_id en el servidor es:", result.usuario.id_docuestudiante)
         // Devuelve el usuario, el rol y el token
         res.status(200).json({
             message: 'Inicio de sesión exitoso',
             user: result.usuario,
             rol: result.rol,
-            user_id: result.id_docuestudiante,
+            user_id: result.usuario.id_docuestudiante,
             token: token
         });
+
+        
     } catch (error) {
         console.error('Error al procesar la solicitud de inicio de sesión:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
